a6eb75ce47d506908970015733137f12f1819001
Hector Melendez <hector.melendez129@gmail.com>
2025-09-27 12:16:06 -0400

    game works

--- js/main.js
+++ js/main.js
@@ -1,154 +1,19 @@
 //activate listener for boxes
-
-const box1 = document.querySelector('.box1')
-box1.addEventListener('click', xoSelector1)
-
-const box2 = document.querySelector('.box2')
-box2.addEventListener('click', xoSelector2)
-
-const box3 = document.querySelector('.box3')
-box3.addEventListener('click', xoSelector3)
-
-const box4 = document.querySelector('.box4')
-box4.addEventListener('click', xoSelector4)
-
-const box5 = document.querySelector('.box5')
-box5.addEventListener('click', xoSelector5)
-
-const box6 = document.querySelector('.box6')
-box6.addEventListener('click', xoSelector6)
-
-const box7 = document.querySelector('.box7')
-box7.addEventListener('click', xoSelector7)
-
-const box8 = document.querySelector('.box8')
-box8.addEventListener('click', xoSelector8)
-
-const box9 = document.querySelector('.box9')
-box9.addEventListener('click', xoSelector9)
-
-//Add XO into boxes - FIND BETTER WAY (OOP??)
-//Activate x and o toggle
+//create array
 let i = 0;
 
-function xoSelector1() {
-    i += 1;
+const boxes = document.querySelectorAll('.box')
+boxes.forEach(x => {
+    x.addEventListener('click', xoToggle)
+});
+
+function xoToggle() {
+    i++;
     if (i % 2 === 0) {
-        box1.innerHTML = 'o';
-
+        this.innerText = 'O';
     } else {
-        box1.innerHTML = 'x';
-
+        this.innerText = 'X';
     }
-    console.log(box1.innerHTML)
-    winMechanics();
-}
-
-function xoSelector2() {
-    i += 1;
-    if (i % 2 === 0) {
-        box2.innerHTML = 'o';
-
-    } else {
-        box2.innerHTML = 'x';
-
-    }
-    console.log(box2.innerHTML)
-    winMechanics();
-}
-
-function xoSelector3() {
-    i += 1;
-    if (i % 2 === 0) {
-        box3.innerHTML = 'o';
-
-    } else {
-        box3.innerHTML = 'x';
-
-    }
-    console.log(box3.innerHTML)
-    winMechanics();
-
-}
-
-function xoSelector4() {
-    i += 1;
-    if (i % 2 === 0) {
-        box4.innerHTML = 'o';
-
-    } else {
-        box4.innerHTML = 'x';
-
-    }
-    console.log(box4.innerHTML)
-    winMechanics();
-
-}
-
-function xoSelector5() {
-    i += 1;
-    if (i % 2 === 0) {
-        box5.innerHTML = 'o';
-
-    } else {
-        box5.innerHTML = 'x';
-
-    }
-    console.log(box5.innerHTML)
-    winMechanics();
-
-}
-
-function xoSelector6() {
-    i += 1;
-    if (i % 2 === 0) {
-        box6.innerHTML = 'o';
-
-    } else {
-        box6.innerHTML = 'x';
-
-    }
-    console.log(box6.innerHTML)
-    winMechanics();
-
-}
-
-function xoSelector7() {
-    i += 1;
-    if (i % 2 === 0) {
-        box7.innerHTML = 'o';
-
-    } else {
-        box7.innerHTML = 'x';
-
-    }
-    console.log(box7.innerHTML)
-    winMechanics();
-
-}
-
-function xoSelector8() {
-    i += 1;
-    if (i % 2 === 0) {
-        box8.innerHTML = 'o';
-
-    } else {
-        box8.innerHTML = 'x';
-
-    }
-    console.log(box8.innerHTML)
-    winMechanics();
-
-}
-
-function xoSelector9() {
-    i += 1;
-    if (i % 2 === 0) {
-        box9.innerHTML = 'o';
-    } else {
-        box9.innerHTML = 'x';
-    }
-    console.log(box9.innerHTML)
     winMechanics();
 }
 
@@ -158,50 +23,49 @@ const gameStatus = document.getElementById('gameStatus');
 
 function winMechanics() {
     // create a switch case that takes in the value of 
-    // box[i].innerhtml text and evaluates to the other boxes
-    // make rule for empty boxes 
+    // box[i].innerText text and evaluate to the other boxes
+    // make rule to ignore empty boxes 
 
     switch (true) {
         //Row Logic
 
-        case (box1.innerHTML !== '' && box1.innerHTML === box2.innerHTML && box2.innerHTML === box3.innerHTML):
-            gameStatus.innerText = 'Top Row Won'
+        case (boxes[0].innerText !== '' && boxes[0].innerText === boxes[1].innerText && boxes[1].innerText === boxes[2].innerText):
+            gameStatus.innerText = `${boxes[0].innerText} Wins! Top Row`;
+
             break
-        case (box4.innerHTML !== '' && box4.innerHTML === box5.innerHTML && box5.innerHTML === box6.innerHTML):
-            gameStatus.innerText = 'Middle Row Won'
+        case (boxes[3].innerText !== '' && boxes[3].innerText === boxes[4].innerText && boxes[4].innerText === boxes[5].innerText):
+            gameStatus.innerText = `${boxes[3].innerText} Wins! Middle Row`
             break
-        case (box7.innerHTML !== '' && box7.innerHTML === box8.innerHTML && box8.innerHTML === box9.innerHTML):
-            gameStatus.innerText = 'bottom row'
+        case (boxes[6].innerText !== '' && boxes[6].innerText === boxes[7].innerText && boxes[7].innerText === boxes[8].innerText):
+            gameStatus.innerText = `${boxes[6].innerText} Wins! Bottom Row`
             break
 
         //Column Logic
 
-        case (box1.innerHTML !== '' && box1.innerHTML === box4.innerHTML && box4.innerHTML === box7.innerHTML):
-            gameStatus.innerText = 'column 1 Won'
+        case (boxes[0].innerText !== '' && boxes[0].innerText === boxes[3].innerText && boxes[3].innerText === boxes[6].innerText):
+            gameStatus.innerText = `${boxes[0].innerText} Wins! Column 1`
             break
-        case (box2.innerHTML !== '' && box2.innerHTML === box5.innerHTML && box5.innerHTML === box8.innerHTML):
-            gameStatus.innerText = 'Column 2 Won'
+        case (boxes[1].innerText !== '' && boxes[1].innerText === boxes[4].innerText && boxes[4].innerText === boxes[7].innerText):
+            gameStatus.innerText = `${boxes[1].innerText} Wins! Column 2`
             break
-        case (box3.innerHTML !== '' && box3.innerHTML === box6.innerHTML && box6.innerHTML === box9.innerHTML):
-            gameStatus.innerText = 'Column 3 Won'
+        case (boxes[2].innerText !== '' && boxes[2].innerText === boxes[5].innerText && boxes[5].innerText === boxes[8].innerText):
+            gameStatus.innerText = `${boxes[2].innerText} Wins! Column 3`
             break
 
         //Diagnal logic
 
-        case (box1.innerHTML !== '' && box1.innerHTML === box5.innerHTML && box5.innerHTML === box9.innerHTML):
-            gameStatus.innerText = 'daignal 1-9'
+        case (boxes[0].innerText !== '' && boxes[0].innerText === boxes[4].innerText && boxes[4].innerText === boxes[8].innerText):
+            gameStatus.innerText = `${boxes[0].innerText} Wins! Diagnal`
             break
-        case (box3.innerHTML !== '' && box3.innerHTML === box5.innerHTML && box5.innerHTML === box7.innerHTML):
-            gameStatus.innerText = 'diagnal 3-7'
+        case (boxes[2].innerText !== '' && boxes[2].innerText === boxes[4].innerText && boxes[4].innerText === boxes[6].innerText):
+            gameStatus.innerText = `${boxes[2].innerText} Wins! Diagnal`
             break
 
-        case (box1.innerHTML !== '' && box2.innerHTML !== '' && box3.innerHTML !== '' && 
-            box4.innerHTML !== '' && box5.innerHTML !== '' && box6.innerHTML !== '' && 
-            box7.innerHTML !== '' && box8.innerHTML !== '' && box9.innerHTML !== ''):
-            gameStatus.innerHTML = 'Draw, Please Reset The Game!'
-        //set default to draw
+        case (boxes[0].innerText !== '' && boxes[1].innerText !== '' && boxes[2].innerText !== '' &&
+            boxes[3].innerText !== '' && boxes[4].innerText !== '' && boxes[5].innerText !== '' &&
+            boxes[6].innerText !== '' && boxes[7].innerText !== '' && boxes[8].innerText !== ''):
+            gameStatus.innerText = 'Draw, Please Reset The Game!'
         default:
-            gameStatus.innerHTML = 'Nice Move!'
             break;
     }
 }
@@ -211,23 +75,10 @@ function winMechanics() {
 document.querySelector('#resetBtn').addEventListener('click', reset)
 //Reset All Boxes To undefined
 function reset() {
-    box1.innerHTML = '';
 
-    box2.innerHTML = '';
-
-    box3.innerHTML = '';
-
-    box4.innerHTML = '';
-
-    box5.innerHTML = '';
-
-    box6.innerHTML = '';
-
-    box7.innerHTML = '';
-
-    box8.innerHTML = '';
-
-    box9.innerHTML = '';
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].innerText = '';
+    }
     gameStatus.innerText = 'Status: '; //reset game status
-    i = 0; //reset toggle
+    i = 0;
 }
